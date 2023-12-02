7f2ac04a6749af22dc3aec5128628cc45fa83a60
bpalmer <benpalmer3@gmail.com>
2023-12-01 21:20:13 -0500

    Day 1 Part 2

--- day1/main.py
+++ day1/main.py
@@ -1,13 +1,40 @@
 from typing import List
 
+# sevenine -> se7en9ne
+# nineight -> n9nei8ht
+# fiveight -> f5veig8t
+# twone -> t2o1e
+# oneight -> o1ei8ht
+# eightwo -> ei8ht2o
+word_to_number = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "thr3e",
+    "four": "fo4r",
+    "five": "f5ve",
+    "six": "s6x",
+    "seven": "se7en",
+    "eight": "ei8ht",
+    "nine": "n9ne",
+}
+
+def replace_numbers(line: str) -> str:
+    for word, repl in word_to_number.items():
+        line = line.replace(word, repl)
+    return line
+
+
 def get_calibration_value(line: str) -> int:
+    line = replace_numbers(line)
     digits = list(filter(lambda c: c.isdigit(), line))
     first = digits[0]
     last = digits[~0]
     return int(first + last)
 
+
 def sum_calibration_values(document: List[str]):
     return sum(list(map(get_calibration_value, document)))
+
 
 if __name__ == "__main__":
     with open("./input") as f:
--- day1/test.py
+++ day1/test.py
@@ -1,5 +1,7 @@
 from main import *
+from typing import List
 import pytest
+
 
 @pytest.mark.parametrize(
     "line,expected_value",
@@ -7,18 +9,44 @@ import pytest
         ("12345", 15),
         ("ab3lda0", 30),
         ("a1b2c3d4e5f", 15),
-    ]
+        ("two1nine", 29),
+        ("zoneight234", 14),
+        ("q35bvblmmqhmnine5zeightwoj", 32),
+        ("twone3943sevenine", 29),
+        ("nineight", 98),
+        ("fiveight", 58),
+        ("oneight", 18),
+    ],
 )
 def test_get_calibration_value(line: str, expected_value: int) -> None:
     assert get_calibration_value(line) == expected_value
 
 
-def test_sum_calibration_values() -> None:
-    document = [
-        "1abc2",
-        "pqr3stu8vwx",
-        "a1b2c3d4e5f",
-        "treb7uchet",
-        ]
-
-    assert sum_calibration_values(document) == 142
+@pytest.mark.parametrize(
+    "document,expected_sum",
+    [
+        (
+            [
+                "1abc2",
+                "pqr3stu8vwx",
+                "a1b2c3d4e5f",
+                "treb7uchet",
+            ],
+            142,
+        ),
+        (
+            [
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen",
+            ],
+            281,
+        )
+    ],
+)
+def test_sum_calibration_values(document: List[str], expected_sum: int) -> None:
+    assert sum_calibration_values(document) == expected_sum
