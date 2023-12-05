2415d0a3c10584b887950ef3f90088cef09c15f7
bpalmer <benpalmer3@gmail.com>
2023-12-05 02:18:29 -0500

    Day 5 Part 2

--- day5/main.py
+++ day5/main.py
@@ -39,8 +39,32 @@ class Map:
         else:
             return destination
 
+    def unmap(self, destination: int) -> int:
+        source = -1
+        for r in self.ranges:
+            if destination in range(r.destination, r.destination + r.length):
+                source = destination - r.destination + r.source
+        if source == -1:
+            return destination
+        else:
+            return source
+
+    def interesting_numbers(self, later_map: "Map", destination_numbers: List[int]):
+        interesting_numbers = []
+        for r in self.ranges:
+            interesting_numbers += [r.source, r.source + r.length]
+        for r in later_map.ranges:
+            interesting_numbers += [
+                self.unmap(r.source),
+                self.unmap(r.source + r.length),
+            ]
+        for number in destination_numbers:
+            interesting_numbers.append(self.unmap(number))
+        return interesting_numbers
+
 
 if __name__ == "__main__":
+    # part 1
     with open("input") as f:
         input = f.read()
     split = input.split("\n\n")
@@ -58,3 +82,38 @@ if __name__ == "__main__":
     print(f"Seeds: {seeds}")
     print(f"Locations: {locations}")
     print(f"Minimum location: {min(locations)}")
+
+    def func(s):
+        source = s
+        for map in maps:
+            source = map.map(source)
+        return source
+
+    # part 2
+    print("\n\nPart 2")
+    locations_map = maps[-1]
+    interesting_numbers = []
+    for i in range(len(maps) - 2, -1, -1):
+        print(f"Converting map {i + 1} to map {i}")
+        print(f"{interesting_numbers=}")
+        interesting_numbers = maps[i].interesting_numbers(
+            maps[i + 1], interesting_numbers
+        )
+        interesting_numbers = sorted(list(set(interesting_numbers)))
+    print(f"Interesting numbers {interesting_numbers}")
+    part2_seeds = []
+    part2_locations = []
+
+    for i in range(0, len(seeds), 2):
+        a = seeds[i]
+        b = seeds[i] + seeds[i + 1]
+        print(f"({a}, {b})")
+        for num in interesting_numbers:
+            if num in range(a, b):
+                print(f"Interesting seed {num}")
+                source = num
+                for map in maps:
+                    source = map.map(source)
+                part2_locations.append(source)
+    print(f"Locations: {part2_locations}")
+    print(f"Minimum location: {min(part2_locations)}")
