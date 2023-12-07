435aaea24d381167c7c47031602471e2661b2036
bpalmer <benpalmer3@gmail.com>
2023-12-07 08:20:28 -0500

    Day 7 Part 1

---
+++ day7/main.py
@@ -0,0 +1,108 @@
+from dataclasses import dataclass
+from enum import IntEnum, auto
+from typing import List
+from functools import total_ordering
+
+
+class Card(IntEnum):
+    c2 = auto()
+    c3 = auto()
+    c4 = auto()
+    c5 = auto()
+    c6 = auto()
+    c7 = auto()
+    c8 = auto()
+    c9 = auto()
+    cT = auto()
+    cJ = auto()
+    cQ = auto()
+    cK = auto()
+    cA = auto()
+
+    @classmethod
+    def from_str(cls, card_str: str) -> "Card":
+        attr = f"c{card_str}"
+        return cls(getattr(cls, attr).value)
+
+    def __repr__(self) -> str:
+        return self._name_.removeprefix("c")
+
+
+class HandType(IntEnum):
+    HighCard = auto()
+    OnePair = auto()
+    TwoPair = auto()
+    ThreeKind = auto()
+    FullHouse = auto()
+    FourKind = auto()
+    FiveKind = auto()
+
+
+@total_ordering
+@dataclass
+class Hand:
+    cards: List[Card]
+    bid: int
+
+    @classmethod
+    def from_str(cls, hand_str: str) -> "Hand":
+        cards, bid = hand_str.split(" ")
+        cards = [Card.from_str(card) for card in cards]
+        bid = int(bid)
+        return cls(cards, bid)
+
+    def get_type(self) -> HandType:
+        card_counts = {card: 0 for card in Card}
+        for card in self.cards:
+            card_counts[card] += 1
+
+        if 5 in card_counts.values():
+            return HandType.FiveKind
+        elif 4 in card_counts.values():
+            return HandType.FourKind
+        elif 3 in card_counts.values() and 2 in card_counts.values():
+            return HandType.FullHouse
+        elif 3 in card_counts.values():
+            return HandType.ThreeKind
+        elif list(card_counts.values()).count(2) == 2:
+            return HandType.TwoPair
+        elif 2 in card_counts.values():
+            return HandType.OnePair
+        else:
+            return HandType.HighCard
+
+    def __eq__(self, other: "Hand") -> bool:
+        return self.cards == other.cards
+
+    def __lt__(self, other: "Hand") -> bool:
+        this_type = self.get_type()
+        other_type = other.get_type()
+        if this_type < other_type:
+            return True
+        elif this_type > other_type:
+            return False
+
+        for this_card, other_card in zip(self.cards, other.cards):
+            if this_card < other_card:
+                return True
+            elif this_card > other_card:
+                return False
+        return False
+
+
+def main() -> None:
+    with open("input") as f:
+        lines = f.read().split("\n")
+    hands = [Hand.from_str(hand_str) for hand_str in lines if hand_str != ""]
+    sorted_hands = sorted(hands)
+    total = 0
+    for i, hand in enumerate(sorted_hands):
+        rank = i + 1
+        winnings = rank * hand.bid
+        total += winnings
+        print(f"Winnings for hand {hand}: {winnings}")
+    print(f"Total winnings: {total}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day7/test.py
@@ -0,0 +1,65 @@
+from main import Card, HandType, Hand
+import pytest
+
+
+@pytest.mark.parametrize(
+    "hand_str,hand",
+    [
+        ("32T3K 765", Hand([Card.c3, Card.c2, Card.cT, Card.c3, Card.cK], 765)),
+        ("KTJJT 220", Hand([Card.cK, Card.cT, Card.cJ, Card.cJ, Card.cT], 220)),
+    ],
+)
+def test_hand_from_str(hand_str: str, hand: Hand) -> None:
+    assert Hand.from_str(hand_str) == hand
+
+
+@pytest.mark.parametrize(
+    "hand,hand_type",
+    [
+        (Hand([Card.c3, Card.c2, Card.cT, Card.c3, Card.cK], 765), HandType.OnePair),
+        (Hand([Card.cT, Card.c5, Card.c5, Card.cJ, Card.c5], 684), HandType.ThreeKind),
+        (Hand([Card.cK, Card.cK, Card.c6, Card.c7, Card.c7], 28), HandType.TwoPair),
+        (Hand([Card.cK, Card.cT, Card.cJ, Card.cJ, Card.cT], 220), HandType.TwoPair),
+        (Hand([Card.cQ, Card.cQ, Card.cQ, Card.cJ, Card.cA], 483), HandType.ThreeKind),
+        (Hand([Card.cQ, Card.cQ, Card.cQ, Card.cA, Card.cA], 899), HandType.FullHouse),
+        (Hand([Card.c4, Card.c4, Card.c4, Card.c4, Card.c4], 473), HandType.FiveKind),
+        (Hand([Card.c2, Card.c3, Card.c4, Card.c5, Card.c6], 58), HandType.HighCard),
+    ],
+)
+def test_hand_type(hand: Hand, hand_type: HandType) -> None:
+    assert hand.get_type() == hand_type
+
+
+@pytest.mark.parametrize(
+    "hand1,hand2,expected",
+    [
+        (Hand([Card.cA] * 5, 25), Hand([Card.cA] * 5, 36), 0),
+        (
+            Hand([Card.cK, Card.cK, Card.c6, Card.c7, Card.c7], 28),
+            Hand([Card.cK, Card.cT, Card.cJ, Card.cJ, Card.cT], 220),
+            1,
+        ),
+        (
+            Hand([Card.cQ, Card.cQ, Card.cQ, Card.cJ, Card.cA], 483),
+            Hand([Card.cQ, Card.cQ, Card.cQ, Card.cA, Card.cA], 292),
+            -1,
+        ),
+        (
+            Hand([Card.cT, Card.c5, Card.c5, Card.cJ, Card.c5], 1),
+            Hand([Card.cK, Card.cK, Card.c6, Card.c7, Card.c7], 28),
+            1,
+        ),
+        (
+            Hand([Card.cT, Card.c5, Card.c5, Card.cJ, Card.c5], 8),
+            Hand([Card.c3, Card.c2, Card.cT, Card.c3, Card.cK], 9),
+            1,
+        ),
+    ],
+)
+def test_hand_cmp(hand1: Hand, hand2: Hand, expected: int) -> None:
+    if expected < 0:
+        assert hand1 < hand2
+    if expected == 0:
+        assert hand1 == hand2
+    if expected > 0:
+        assert hand1 > hand2
