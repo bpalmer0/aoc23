3344b3536a825141a5afa2f88611ef7286ba2f8e
bpalmer <benpalmer3@gmail.com>
2023-12-08 12:56:38 -0500

    Day 8 Part 2

--- day8/main.py
+++ day8/main.py
@@ -1,3 +1,6 @@
+from math import lcm
+
+
 def main():
     with open("input") as f:
         lines = f.read().split("\n")
@@ -14,17 +17,36 @@ def main():
         nodes[name] = destinations
     print(directions, nodes)
 
-    current_node = "AAA"
-    steps = 0
-    while current_node != "ZZZ":
-        next = nodes[current_node]
-        direction = directions[steps % len(directions)]
-        if direction == "R":
-            current_node = next[1]
-        else:
-            current_node = next[0]
-        steps += 1
-    print(f"ZZZ reached in {steps} steps")
+    starting_nodes = [node for node in nodes.keys() if node.endswith("A")]
+    print(f"{starting_nodes=}")
+    ending_nodes = [node for node in nodes.keys() if node.endswith("Z")]
+    print(f"{ending_nodes=}")
+    periods = []
+    for starting_node in starting_nodes:
+        steps = 0
+        z_node_steps = []
+        current_node = starting_node
+        while steps <= 100000:
+            next = nodes[current_node]
+            direction = directions[steps % len(directions)]
+            if direction == "R":
+                next_node = next[1]
+            else:
+                next_node = next[0]
+            steps += 1
+            if next_node.endswith("Z"):
+                z_node_steps.append((steps, next_node))
+            current_node = next_node
+        print(f"Starting node {starting_node} reached Z nodes at {z_node_steps}")
+        differences = []
+        for i, (steps, node) in enumerate(z_node_steps):
+            if i == 0:
+                continue
+            differences.append(steps - z_node_steps[i - 1][0])
+        print(f"Differences {differences}")
+        periods.append(differences[-1])
+    steps = lcm(*periods)
+    print(f"All destination nodes reached in {steps} steps")
 
 
 if __name__ == "__main__":
