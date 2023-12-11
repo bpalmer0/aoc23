bb58013721e1c17880818a43ec01b3603edaac86
bpalmer <benpalmer3@gmail.com>
2023-12-11 18:00:13 -0500

    Day 11 Part 2

--- day11/main.py
+++ day11/main.py
@@ -5,45 +5,49 @@ from typing import List
 def main():
     with open("input") as f:
         lines = f.read().strip().split("\n")
-    grid = []
-    for line in lines:
-        grid.append([c for c in line])
-
-    empty_cols: List[int] = []
-    for col in range(len(grid[0])):
-        column = [row[col] for row in grid]
-        if "#" not in column:
-            empty_cols.append(col)
-    empty_cols = sorted(empty_cols, reverse=True)
-    print(f"empty cols: {empty_cols}")
-    for col in empty_cols:
-        for row in grid:
-            row.insert(col, ".")
-    grid_width = len(grid[0])
-
-    empty_rows = []
-    for r, row in enumerate(grid):
-        if "#" not in row:
-            empty_rows.append(r)
-    empty_rows = sorted(empty_rows, reverse=True)
-    print(f"empty rows: {empty_rows}")
-    for empty_row in empty_rows:
-        grid.insert(empty_row, grid_width * ["."])
-
-    for row in grid:
-        print(row)
-
     galaxies = []
-    for r, row in enumerate(grid):
+    grid_height = len(lines)
+    grid_width = len(lines[0])
+    for r, row in enumerate(lines):
         for c, char in enumerate(row):
             if char == "#":
                 galaxies.append((c, r))
-    print(f"Found {len(galaxies)} galaxies")
+    print(galaxies)
 
-    sum = 0
+    galaxy_rows = [galaxy[1] for galaxy in galaxies]
+    empty_rows = [r for r in range(grid_height) if r not in galaxy_rows]
+    empty_rows.sort(reverse=True)
+
+    empty_space_distance = 1000000
+
+    for empty_row in empty_rows:
+        new_galaxies = []
+        for c, r in galaxies:
+            if r > empty_row:
+                new_galaxies.append((c, r + empty_space_distance - 1))
+            else:
+                new_galaxies.append((c, r))
+        galaxies = new_galaxies
+
+    galaxy_columns = [galaxy[0] for galaxy in galaxies]
+    empty_columns = [c for c in range(grid_height) if c not in galaxy_columns]
+    empty_columns.sort(reverse=True)
+
+    for empty_column in empty_columns:
+        new_galaxies = []
+        for c, r in galaxies:
+            if c > empty_column:
+                new_galaxies.append((c + empty_space_distance - 1, r))
+            else:
+                new_galaxies.append((c, r))
+        galaxies = new_galaxies
+
     print(len(list(combinations(galaxies, 2))))
+    sum = 0
     for g1, g2 in combinations(galaxies, 2):
-        sum += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+        x_diff = abs(g1[0] - g2[0])
+        y_diff = abs(g1[1] - g2[1])
+        sum += x_diff + y_diff
     print(f"Sum of all shortest paths between all pairs of galaxies: {sum}")
 
 
