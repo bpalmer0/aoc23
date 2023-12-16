61bebfb0c718fb17149facdd369fbb6b08bc09f7
bpalmer <benpalmer3@gmail.com>
2023-12-16 18:53:59 -0500

    Day 16 Part 2

--- day16/main.py
+++ day16/main.py
@@ -14,6 +14,23 @@ def display_grid() -> None:
         print(row)
 
 
+def clear_grid() -> None:
+    global active_splitters
+    active_splitters = []
+    for number_row in number_grid:
+        for i in range(len(number_row)):
+            number_row[i] = 0
+
+
+def get_num_energized() -> int:
+    num_energized = 0
+    for number_row in number_grid:
+        for num in number_row:
+            if num > 0:
+                num_energized += 1
+    return num_energized
+
+
 def beam(beam_x: int, beam_y: int, beam_vx: int, beam_vy: int) -> None:
     while True:
         beam_x += beam_vx
@@ -25,11 +42,8 @@ def beam(beam_x: int, beam_y: int, beam_vx: int, beam_vy: int) -> None:
             or beam_y < 0
         ):
             break
-        # print(f"{beam_x=}, {beam_y=}")
         tile = grid[beam_y][beam_x]
         number_grid[beam_y][beam_x] += 1
-        print(f"\n")
-        display_grid()
         if tile == ".":
             continue
         elif tile == "\\":
@@ -90,14 +104,42 @@ def main():
             grid.append(row)
             number_grid.append(number_row)
 
+        print("Part 1")
         beam(-1, 0, 1, 0)
+        print(f"Number of energized tiles: {get_num_energized()}")
+        clear_grid()
+        beam(-1, 0, 1, 0)
+        print(f"Number of energized tiles: {get_num_energized()}")
 
-        num_energized = 0
-        for number_row in number_grid:
-            for num in number_row:
-                if num > 0:
-                    num_energized += 1
-        print(f"Number of energized tiles: {num_energized}")
+        print("Part 2")
+        max_num_energized = 0
+        for r in range(len(grid)):
+            # enter from left
+            clear_grid()
+            beam(-1, r, 1, 0)
+            energized = get_num_energized()
+            print(f"{energized=}")
+            max_num_energized = max(max_num_energized, get_num_energized())
+            # enter from right
+            clear_grid()
+            beam(len(grid[0]), r, -1, 0)
+            energized = get_num_energized()
+            print(f"{energized=}")
+            max_num_energized = max(max_num_energized, get_num_energized())
+        for c in range(len(grid[0])):
+            # enter from top
+            clear_grid()
+            beam(c, -1, 0, 1)
+            energized = get_num_energized()
+            print(f"{energized=}")
+            max_num_energized = max(max_num_energized, get_num_energized())
+            # enter from bottom
+            clear_grid()
+            beam(c, len(grid), 0, -1)
+            energized = get_num_energized()
+            print(f"{energized=}")
+            max_num_energized = max(max_num_energized, get_num_energized())
+        print(f"Max number of energized tiles: {max_num_energized}")
 
 
 if __name__ == "__main__":
